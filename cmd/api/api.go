040f9bf1e88881233bc5e966a0c7e4dd374f1bad
ahmed medhat <ahmed.medhat.saad77@gmail.com>
2026-03-24 05:53:48 +0200

    add auth routes login/register & Makefile added with proper commands to build, test and run

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/ahMEDhat-7/go-api/services/user"
 	"github.com/gorilla/mux"
 )
 
@@ -22,12 +23,11 @@ func NewApiServer(addr string , db *sql.DB) *APIServer{
 	}
 }
 
-
 func (apiServer *APIServer ) Run() error{
-	
 	router := mux.NewRouter()
-	subRouter := router.PathPrefix("/api/vi").Subrouter()
-	
+	subRouter := router.PathPrefix("/api/v1").Subrouter()
+	userHandler := user.NewHandler()
+	userHandler.RegisterRoutes(subRouter)
 	log.Println("Listening On",apiServer.addr)
 	return http.ListenAndServe(apiServer.addr,router)
 }
--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"log"
+
+	"github.com/ahMEDhat-7/go-api/cmd/api"
+)
+
+
+
+func main(){
+
+	server := api.NewApiServer(":8000",nil)
+	if err := server.Run() ; err != nil{
+		log.Fatal(err)
+	}
+}
